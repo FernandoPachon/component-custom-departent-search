a12be13927cfce224caef861f249afd6c78c8029
Fernando Pachon <josep3194@gmail.com>
2022-11-25 11:15:37 -0500

    feat:complete component

--- react/components/DepartmentSearch.tsx
+++ react/components/DepartmentSearch.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import { useQuery } from 'react-apollo';
-import QUERY_VALUE from '/graphql/getDepartmentGroup.graphql'
+import React, { useState } from 'react'
+import { useQuery } from 'react-apollo'
+import DepartmentGroup from './DepartmentGroup'
+import getCategories from '../graphql/getDepartmentGroup.graphql'
 
-//import { SearchBar } from 'vtex.store-components'
+import { SearchBar } from 'vtex.store-components'
 
-const DepartmentSearch =()=>{
-  const { data,loading }=useQuery(QUERY_VALUE)
-  console.log("mis datos de query son",data,loading);
-  return loading ?<div> Loading... </div> :<div> Búsqueda de departamento </div>
+const DepartmentSearch = () => {
+  const { data, loading } = useQuery(getCategories)
 
+  console.log('mis datos de query son', data?.categories[0]?.children)
+  const [slug, setSlug] = useState('')
+
+  console.log('mi slug seleccionado es :', slug)
+
+  return loading ? (
+    <div> Loading... </div>
+  ) : (
+    <div>
+      <DepartmentGroup
+        departments={data?.categories[0]?.children}
+        handleSetSlug={setSlug}
+      />
+      <SearchBar
+        customSearchPageUrl={slug}
+        placeholder="¿Que estas buscando?"
+        displayMode="search-and-clear-buttons"
+        containerMode='container'
+      />
+
+    </div>
+  )
 }
-export default DepartmentSearch;
+export default DepartmentSearch
